8db4f963bc145828057257ee0297f2fe8c6c2034
Aram Song <97036411+aramssong@users.noreply.github.com>
2022-10-21 08:50:24 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+N = int(input())
+num = list(map(int, input().split()))
+
+answer = 0
+
+for i in num:
+    count = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            count += 1
+    if count == 2:
+        answer += 1
+
+print(answer)    
