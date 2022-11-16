3d93c12a648a6919fe5750926a2dcf2e66bc3883
Aram Song <97036411+aramssong@users.noreply.github.com>
2022-11-16 09:09:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,6 +1,9 @@
-import math
+def factorial(n):
+    num = 1
+    for i in range(2, n+1):
+        num *= i
+    return num
 
 N, K = map(int, input().split())
-
-answer = math.factorial(N) // (math.factorial(N-K)*math.factorial(K))
+answer = factorial(N) // (factorial(N-K) * factorial(K))
 print(answer)
